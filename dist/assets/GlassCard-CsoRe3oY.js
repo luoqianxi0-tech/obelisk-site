10131d2c8ef03acd2cc38667467b61ec1fb2c544
luoqianxi0-tech <luoqianxi0@gmail.com>
2026-06-18 21:26:09 +0800

    Add files via upload

---
+++ dist/assets/GlassCard-CsoRe3oY.js
@@ -0,0 +1,2 @@
+import{j as s,m as o}from"./index-DCLf2-AC.js";function e({children:t,className:i="",onClick:a,delay:r=0}){return s.jsx(o.div,{initial:{opacity:0,y:20},animate:{opacity:1,y:0},transition:{duration:.5,delay:r},onClick:a,className:`glass-card rounded-2xl p-6 ${a?"cursor-pointer":""} ${i}`,children:t})}export{e as G};
+//# sourceMappingURL=GlassCard-CsoRe3oY.js.map
