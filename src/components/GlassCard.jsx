974a64571906d0662ec965faa920eee0a7815982
luoqianxi0-tech <luoqianxi0@gmail.com>
2026-06-24 21:58:59 +0800

    Delete src directory

--- src/components/GlassCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function GlassCard({ children, className, strong }) {
-  return (
-    <div className={`${strong ? 'glass-strong' : 'glass'} p-5 ${className || ''}`}>
-      {children}
-    </div>
-  )
-}
