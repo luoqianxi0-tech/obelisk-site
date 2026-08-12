f98d3162c61dea82ef20ebdc6a852691b434504f
luoqianxi0-tech <luoqianxi0@gmail.com>
2026-08-12 14:06:58 +0800

    Delete OBELISK_v11 directory

--- OBELISK_v11/src/components/ObeliskIcon.jsx
+++
@@ -1,6 +0,0 @@
-export const ObeliskIcon = ({ className = "w-8 h-8" }) => (
-  <svg viewBox="0 0 100 100" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="100" height="100" fill="white"/>
-    <polygon points="50,15 85,80 15,80" fill="black"/>
-  </svg>
-);
